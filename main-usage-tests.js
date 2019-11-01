928b6158d6f1f865f97504e4bde83236005949fc
Rosano Coutinho <pub@xeari.com>
2019-11-01 17:15:34 -0400

    Tidy specs

--- main-usage-tests.js
+++ main-usage-tests.js
@@ -16,44 +16,48 @@ describe('MSTMassage_Usage', function testMSTMassage_Usage() {
 		deepEqual(MSTMassage('alfa', '$input'), 'alfa');
 	});
 
-	it('$input', function () {
-		deepEqual(MSTMassage('alfa', '$input'), 'alfa');
+	context('string', function () {
+
+		it('match bool false', function () {
+			deepEqual(MSTMassage('alfa', '$input.isMatch(/bravo/)'), 'false');
+		});
+
+		it('match bool true', function () {
+			deepEqual(MSTMassage('alfa', '$input.isMatch(/alfa/)'), 'true');
+		});
+
+		it('match array with no capture', function () {
+			deepEqual(MSTMassage('- alfa\n- bravo\n', '$input.matchArray(/- .*\n/)'), '[]');
+		});
+
+		it('match array with capture with no global', function () {
+			deepEqual(MSTMassage('- alfa\n- bravo\n', '$input.matchArray(/- (.*)\n/)'), JSON.stringify([{ 1: 'alfa' }]));
+		});
+
+		it('match array with capture with global', function () {
+			deepEqual(MSTMassage('- alfa\n- bravo\n', '$input.matchArray(/- (.*)\n/g)'), JSON.stringify([{ 1: 'alfa' }, { 1: 'bravo' }]));
+		});
+
+		it('lines', function () {
+			deepEqual(MSTMassage('alfa\nbravo\n', '$input.lines'), JSON.stringify(['alfa', 'bravo']));
+		});
+	
 	});
 
-	it('string match bool false', function () {
-		deepEqual(MSTMassage('alfa', '$input.isMatch(/bravo/)'), 'false');
-	});
+	context('array', function () {
 
-	it('string match bool true', function () {
-		deepEqual(MSTMassage('alfa', '$input.isMatch(/alfa/)'), 'true');
-	});
+		it('first', function () {
+			deepEqual(MSTMassage('alfa\nbravo\n', '$input.lines.first'), 'alfa');
+		});
 
-	it('string match array with no capture', function () {
-		deepEqual(MSTMassage('- alfa\n- bravo\n', '$input.matchArray(/- .*\n/)'), '[]');
-	});
+		it('last', function () {
+			deepEqual(MSTMassage('alfa\nbravo\n', '$input.lines.last'), 'bravo');
+		});
 
-	it('string match array with capture with no global', function () {
-		deepEqual(MSTMassage('- alfa\n- bravo\n', '$input.matchArray(/- (.*)\n/)'), JSON.stringify([{ 1: 'alfa' }]));
-	});
-
-	it('string match array with capture with global', function () {
-		deepEqual(MSTMassage('- alfa\n- bravo\n', '$input.matchArray(/- (.*)\n/g)'), JSON.stringify([{ 1: 'alfa' }, { 1: 'bravo' }]));
-	});
-
-	it('$input.lines', function () {
-		deepEqual(MSTMassage('alfa\nbravo\n', '$input.lines'), JSON.stringify(['alfa', 'bravo']));
-	});
-
-	it('$input.lines.first', function () {
-		deepEqual(MSTMassage('alfa\nbravo\n', '$input.lines.first'), 'alfa');
-	});
-
-	it('$input.lines.last', function () {
-		deepEqual(MSTMassage('alfa\nbravo\n', '$input.lines.last'), 'bravo');
-	});
-
-	it('$input.lines[1]', function () {
-		deepEqual(MSTMassage('alfa\nbravo\n', '$input.lines[1]'), 'bravo');
+		it('index', function () {
+			deepEqual(MSTMassage('alfa\nbravo\n', '$input.lines[1]'), 'bravo');
+		});
+	
 	});
 
 });
